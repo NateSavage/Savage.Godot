9a8c1d95d1a4f8fad866e0374df20d85d2805eb9
Nate Savage <Nate.Savage@TexelDrain.com>
2024-08-08 23:23:28 -0500

    Removed Unity Section From .gitignore

--- Source/GlobalUsings.cs
+++ Source/GlobalUsings.cs
@@ -5,6 +5,5 @@ global using System;
 global using System.Collections.Generic;
 
 
-
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Savage.Godot.Benchmarks")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Savage.Godot.Tests")]
